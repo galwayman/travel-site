089bc836f21c7e7599a6c1906a0841be3eb35715
AW <aodhan.m.ward@gmail.com>
2020-06-08 17:06:02 +0100

    Add Styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 
+
 const path = require('path')
+
+const postCSSPlugins = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
 module.exports = {
     entry:'./app/assets/scripts/App.js',
     output: {
@@ -8,4 +15,19 @@ module.exports = {
     },
     mode: 'development',
     watch: true,
+    module:{
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                   "style-loader" ,"css-loader", 
+                   {loader:"postcss-loader", 
+                    options:{
+                        plugins: postCSSPlugins
+                            }
+                    }
+                ]
+            }
+        ]
+    }
 }
